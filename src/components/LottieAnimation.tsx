44d934f65e86811b70dfd93fe291f758fd92a11a
Leonardo Henao <leonardofhenao.085@gmail.com>
2024-03-28 22:06:58 -0500

    Feat. Se agrego pagina 404 y se agrego lottie para animaciones

---
+++ src/components/LottieAnimation.tsx
@@ -0,0 +1,12 @@
+import Lottie from 'lottie-react';
+import Anim404 from '../assets/404.json';
+
+const LottieAnimation = () => {
+  return (
+    <>
+      <Lottie animationData={Anim404} loop={true} />
+    </>
+  );
+};
+
+export default LottieAnimation;
