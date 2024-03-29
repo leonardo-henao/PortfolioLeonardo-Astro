6a8b3be7dc8d0d43c8d7593589957cb7fae150a6
Leonardo Henao <leonardofhenao.085@gmail.com>
2024-03-28 21:28:03 -0500

    Feat: Migracion finalizada, se agrego swiper para los slider de los proyectos y se agrego framer motion para animaciones

---
+++ src/data/ContactLinks.ts
@@ -0,0 +1,34 @@
+import type { IContactLinks } from '../interfaces/IContactLinks';
+
+export const ContactLinks: IContactLinks[] = [
+  {
+    link: 'http://wa.me/573165051130',
+    icon: '/icons/whatsapp.svg',
+    alt: 'icono de whatsapp',
+    iconSmall: false,
+  },
+  {
+    link: 'mailto:leonardofhenao.085@gmail.com',
+    icon: '/icons/gmail.svg',
+    alt: 'icono de gmail',
+    iconSmall: false,
+  },
+  {
+    link: 'https://github.com/leonardo-henao',
+    icon: '/icons/github.svg',
+    alt: 'icono de github',
+    iconSmall: true,
+  },
+  {
+    link: 'https://play.google.com/store/apps/dev?id=7404460400144886326&hl=es-419',
+    icon: '/icons/google-play.svg',
+    alt: 'icono de google play store',
+    iconSmall: true,
+  },
+  {
+    link: 'https://www.linkedin.com/in/leonardo-henao/',
+    icon: '/icons/linkedin.svg',
+    alt: 'icono de linkedin',
+    iconSmall: false,
+  },
+];
