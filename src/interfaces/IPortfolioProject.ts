8fdcb301a181e63f7993f85ef93b81ddaeb4729f
Leonardo Henao <leonardofhenao.085@gmail.com>
2024-05-16 18:51:52 -0500

    Feat: Se agrega pagina de social para compartir en redes

--- src/interfaces/IPortfolioProject.ts
+++ src/interfaces/IPortfolioProject.ts
@@ -1,8 +1,8 @@
 export interface IPortfolioProject {
-	name: string;
-	image: string;
-	imageBackground: string;
-	description: string;
-	toolsUsed: string[];
-	link: string;
+  name: string;
+  image: string;
+  imageBackground: string;
+  description: string;
+  toolsUsed: string[];
+  link: string;
 }
